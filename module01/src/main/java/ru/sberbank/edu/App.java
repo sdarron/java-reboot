48bcbdef734d4f089fa56d15a4c34ca4af37edfd
sdarron <zaytcev@bk.ru>
2023-10-17 00:42:21 +0300

    01

--- module01/src/main/java/ru/sberbank/edu/App.java
+++ module01/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,22 @@
 package ru.sberbank.edu;
 
 /**
- * Hello world!
- *
+ * Проект для первого домашнего задания Школы Java разработчиков
+ * @author Зайцев Денис Николевич
+ * @version 1.0
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        GreetingImpl impl = new GreetingImpl();
+        String hobby = impl.getBestHobby();
+        System.out.println( hobby );
+
+        GCD gcd = new GCD();
+        int num = gcd.getDivisor(12, 8);
+        System.out.println(num);
+
+
     }
 }
