4996ef9b05cc7972a3aeacf1b8d82afb82a44014
sdarron <zaytcev@bk.ru>
2023-12-13 17:19:43 +0300

    mod9 init

--- module08/src/main/java/ru/sberbank/edu/FinancialServlet.java
+++ module08/src/main/java/ru/sberbank/edu/FinancialServlet.java
@@ -1,13 +1,25 @@
 package ru.sberbank.edu;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
-public class FinancialServlet
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class FinancialServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("<html>");
+        writer.write("<body>");
+        writer.write("<h1> FinancialServlet </h>");
+        writer.write("</body>");
+        writer.write("</html>");
     }
 }
--- module09/src/main/java/ru/sberbank/edu/App.java
+++ module09/src/main/java/ru/sberbank/edu/App.java
@@ -1,13 +1,22 @@
 package ru.sberbank.edu;
 
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+
+        WeatherCache cache = new WeatherCache(new WeatherProvider());
+        WeatherInfo weatherInfo = cache.getWeatherInfo("Moscow");
+        System.out.println(weatherInfo.toString());
+        sleep(2000);
+        WeatherInfo weatherInfo1 = cache.getWeatherInfo("Moscow");
+        System.out.println(weatherInfo1.toString());
     }
 }
